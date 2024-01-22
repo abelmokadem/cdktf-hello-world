aa887b035f7690f687fa732c343c86bec45628ea
Ash Belmokadem <ash.belmokadem@aegon.nl>
2024-01-22 17:24:52 +0100

    Reset all code

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM nixos/nix:2.19.3
-
-RUN nix-channel --update
-
-COPY default.nix .
-
-RUN nix-build
-
--- default.nix
+++ default.nix
@@ -20,5 +20,7 @@ pkgs.mkShell {
   shellHook = ''
     [[ -d .venv ]] || python -m venv .venv
     source .venv/bin/activate
+
+    pip install -r requirements.txt
   '';
 }
